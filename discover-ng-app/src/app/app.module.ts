a5b6d36d7edea795be646d2b09839b7882d6fb99
Razor <rzrdeyafgh7@gmail.com>
2019-07-12 17:19:09 -0400

    Added NgBootstrap, added navbar.

--- discover-ng-app/src/app/app.module.ts
+++ discover-ng-app/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component'
-import {Socket, SocketIoConfig, SocketIoModule} from "ngx-socket-io";
+import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
 
 const config: SocketIoConfig = {url: 'http://localhost:5000', options: {}};
 
@@ -14,6 +15,7 @@ const config: SocketIoConfig = {url: 'http://localhost:5000', options: {}};
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
     SocketIoModule.forRoot(config)
   ],
   providers: [],

2b58e024678a03d1a4dbb098828c03dd3ef8d0fa
Danil Nagy <DNagy@Silvprop.com>
2019-08-22 17:09:57 -0400

    added script to compile release versions

---
+++ compile-release.bat
@@ -0,0 +1,12 @@
+mkdir discover-release
+
+mkdir discover-release\src
+xcopy .\src .\discover-release\src
+
+mkdir discover-release\static
+xcopy .\static .\discover-release\static
+
+copy start-discover.bat .\discover-release\discover.bat
+copy LICENSE.txt .\discover-release\LICENSE.txt
+copy requirements.txt .\discover-release\requirements.txt
+copy server.py .\discover-release\server.py

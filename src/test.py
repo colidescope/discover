edf10acf35e316287b557eaeeb10a12b4ff3ce8c
danilnagy <4085524+danilnagy@users.noreply.github.com>
2019-11-21 15:31:04 -0500

    updated test function

--- src/test.py
+++ src/test.py
@@ -8,10 +8,12 @@ class Test:
 	def __init__(self, client):
 
 		self.inputs = []
-		self.inputs.append({"id": "in-1", "name": "number", "type": "Continuous", "min": 0, "max": 10, "num": 1})
+		self.inputs.append({"id": "in-1", "name": "x", "type": "Continuous", "min": 0, "max": 5, "num": 1})
+		self.inputs.append({"id": "in-2", "name": "y", "type": "Continuous", "min": 0, "max": 3, "num": 1})
 
 		self.outputs = []
-		self.outputs.append({"id": "out-1", "name": "sum", "type": "Objective", "goal": "Maximize"})
+		self.outputs.append({"id": "out-1", "name": "f1", "type": "Objective", "goal": "Minimize"})
+		self.outputs.append({"id": "out-2", "name": "f2", "type": "Objective", "goal": "Minimize"})
 
 	def get_inputs(self):
 		return self.inputs
@@ -21,11 +23,19 @@ class Test:
 	def get_outputs(self):
 		return self.outputs
 
-	def calculate(self, inputs):
-		output = sum([sum(i) for i in inputs])
-		
-		#process inputs to compute outputs
-		output_def = dict(self.outputs[0])
-		output_def["value"] = output
+	def calculate(self, input_vals):
 
-		return [output_def]
+		x = input_vals[0][0]
+		y = input_vals[1][0]
+
+		f1 = 4 * x ** 2 + 4 * y ** 2
+		f2 = (x-5) ** 2 + (y-5) ** 2 
+		output_vals = [f1, f2]
+
+		outputs_out = []
+
+		for i, o in enumerate(self.outputs):
+			o["value"] = output_vals[i]
+			outputs_out.append(o)
+
+		return outputs_out

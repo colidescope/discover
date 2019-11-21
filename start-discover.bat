cc9accdf0b635adf8491de53fd2dab26099e73b0
danilnagy <4085524+danilnagy@users.noreply.github.com>
2019-11-21 15:10:39 -0500

    changed local run script to use virtual environment

--- start-discover.bat
+++ start-discover.bat
@@ -1,3 +1,13 @@
-pip install -r requirements.txt
-explorer "http://localhost:5000/"
-python server.py
+@echo off
+IF EXIST ".\env" (
+    CALL .\env\Scripts\activate.bat
+    ECHO Discover already installed
+) ELSE (
+    ECHO Installing Discover, this may take a few minutes...
+    CALL python -m venv env
+    CALL .\env\Scripts\activate.bat
+    CALL pip install -r requirements.txt
+    ECHO Discover successfully installed
+)
+CALL explorer "http://localhost:5000/"
+CALL python server.py
